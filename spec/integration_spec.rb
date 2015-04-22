7735b227b1e94b512677ea70e44f45cdafce104a
Leonid Shevtsov <leonid@shevtsov.me>
2015-04-22 20:33:36 +0300

    attempt to make integration tests

---
+++ spec/integration_spec.rb
@@ -0,0 +1,36 @@
+require 'headless'
+require 'selenium-webdriver'
+
+describe 'Integration test' do
+  it 'should start xvfb' do
+    Headless.ly do
+      driver = Selenium::WebDriver.for :firefox
+      driver.navigate.to 'http://google.com'
+      expect(driver.title).to match /Google/
+      driver.close
+    end
+  end
+
+  it 'should record screenshots' do
+    Headless.ly do |headless|
+      driver = Selenium::WebDriver.for :firefox
+      driver.navigate.to 'http://google.com'
+      expect(driver.title).to match /Google/
+      headless.take_screenshot("test.jpg")
+      driver.close
+      expect(File.exists?("test.jpg")).to eq true
+    end
+  end
+
+  it 'should record video with ffmpeg' do
+    Headless.ly do |headless|
+      headless.video.start_capture
+      driver = Selenium::WebDriver.for :firefox
+      driver.navigate.to 'http://google.com'
+      expect(driver.title).to match /Google/
+      driver.close
+      headless.video.stop_and_save("test.mov")
+      expect(File.exists?("test.mov")).to eq true
+    end
+  end
+end
