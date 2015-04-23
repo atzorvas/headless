12b35a303746a732c2a30b4d07bf56b629542fe2
Leonid Shevtsov <leonid@shevtsov.me>
2015-04-23 13:14:51 +0300

    still fixing travis

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -17,7 +17,7 @@ describe 'Integration test' do
   end
 
   it 'should record video with ffmpeg' do
-    `xeyes &` if ENV['TRAVIS'] # fix error with avconv http://superuser.com/a/593534
+    `firefox &` if ENV['TRAVIS'] # fix error with avconv http://superuser.com/a/593534
     headless.video.start_capture
     work_with_browser
     headless.video.stop_and_save("test.mov")
