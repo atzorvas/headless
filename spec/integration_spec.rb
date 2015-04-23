ae2b94f0db1a66ec0ed8594b4f730c1de33ad5c3
Leonid Shevtsov <leonid@shevtsov.me>
2015-04-23 08:18:25 +0300

    spec refactoring

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -3,34 +3,31 @@ require 'selenium-webdriver'
 
 describe 'Integration test' do
   it 'should start xvfb' do
-    Headless.ly(display: 999) do
-      driver = Selenium::WebDriver.for :firefox
-      driver.navigate.to 'http://google.com'
-      expect(driver.title).to match /Google/
-      driver.close
+    Headless.ly do
+      work_with_browser
     end
   end
 
   it 'should record screenshots' do
-    Headless.ly(display: 999) do |headless|
-      driver = Selenium::WebDriver.for :firefox
-      driver.navigate.to 'http://google.com'
-      expect(driver.title).to match /Google/
+    Headless.ly do |headless|
       headless.take_screenshot("test.jpg")
-      driver.close
       expect(File.exists?("test.jpg")).to eq true
     end
   end
 
   it 'should record video with ffmpeg' do
-    Headless.ly(display: 999, video: {log_file_path: STDERR}) do |headless|
+    Headless.ly(video: {log_file_path: STDERR}) do |headless|
       headless.video.start_capture
-      driver = Selenium::WebDriver.for :firefox
-      driver.navigate.to 'http://google.com'
-      expect(driver.title).to match /Google/
-      driver.close
+      work_with_browser
       headless.video.stop_and_save("test.mov")
       expect(File.exists?("test.mov")).to eq true
     end
+  end
+
+  def work_with_browser
+    driver = Selenium::WebDriver.for :firefox
+    driver.navigate.to 'http://google.com'
+    expect(driver.title).to match /Google/
+    driver.close
   end
 end
