b95acab4b2800a87b4de200f9325bef29ea3aae9
Leonid Shevtsov <leonid@shevtsov.me>
2015-04-23 08:39:47 +0300

    fixing integration specs

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -1,33 +1,37 @@
 require 'headless'
 require 'selenium-webdriver'
 
+# In these tests I use different display numbers to avoid
 describe 'Integration test' do
-  it 'should start xvfb' do
-    Headless.ly do
-      work_with_browser
-    end
+  before do
+    @headless = Headless.new
+    @headless.start
+  end
+
+  after do
+    @headless.destroy
+  end
+
+  it 'should use xvfb' do
+    work_with_browser
   end
 
   it 'should record screenshots' do
-    Headless.ly do |headless|
-      headless.take_screenshot("test.jpg")
-      expect(File.exists?("test.jpg")).to eq true
-    end
+    headless.take_screenshot("test.jpg")
+    expect(File.exist?("test.jpg")).to eq true
   end
 
   it 'should record video with ffmpeg' do
-    Headless.ly(video: {log_file_path: STDERR}) do |headless|
-      headless.video.start_capture
-      work_with_browser
-      headless.video.stop_and_save("test.mov")
-      expect(File.exists?("test.mov")).to eq true
-    end
+    headless.video.start_capture
+    work_with_browser
+    headless.video.stop_and_save("test.mov")
+    expect(File.exist?("test.mov")).to eq true
   end
 
   def work_with_browser
     driver = Selenium::WebDriver.for :firefox
     driver.navigate.to 'http://google.com'
-    expect(driver.title).to match /Google/
+    expect(driver.title).to match(/Google/)
     driver.close
   end
 end
